77bdc635238378015fe2bb242417a9584a97fc3c
Elham <elhamabdue49@gmail.com>
2025-08-06 21:58:20 +0300

    finalize task one

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import Header from './assets/Header.jsx';
+
+
+function App() {
+  return(
+    <Header />
+  );
+}
+
+export default App
---
+++ src/assets/Header.jsx
@@ -0,0 +1,19 @@
+function Header(){
+    return(
+        <div>
+            
+        <nav>
+            <ul>
+                <li><a href="#"></a>Home</li>
+                <li>Tasks</li>
+                <li>Explore</li>
+                <li>About</li>
+
+            </ul>
+        </nav>
+        <h1>Welcome to My islamic To-Do App</h1>
+        </div>
+        
+    );
+}
+export default Header
